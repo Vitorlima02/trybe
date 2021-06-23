66b34e77e2d8106d220e90f40b35b84f65a469b2
Vitor Lima <vitorliman@gmail.com>
2021-06-23 18:41:21 -0300

    Primeiro requisito do exercicio

---
+++ exercicios/bloco-4.1/script.js
@@ -0,0 +1,22 @@
+let a = 5;
+let b = 10;
+
+//Adição
+let soma = a + b;
+  console.log(soma);
+
+//Subtração
+let subtracao = a - b;
+  console.log(subtracao);
+
+//Multiplicação
+let mult = a * b;
+  console.log(mult);
+
+//Divisão
+let div = a / b;
+  console.log(div);
+
+//Módulo
+let resto = a % b == 0
+  console.log(resto);
