f7b06c4c37437c7622d9fae478680adf215e2fa8
Vitor Lima <vitorliman@gmail.com>
2021-06-25 22:14:32 -0300

    Exercicios bloco 4.3

---
+++ exercicios/bloco-4.3/triangulo.js
@@ -0,0 +1,12 @@
+//2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+let number = 5;
+let linhas = "";
+
+for (let index = 1; index <= number; index += 1) {
+  linhas += "*";
+  console.log(linhas);
+}
+linhas = "";
+
+
+
