29909796302f55970f9be453536ab310c814beaa
Vitor Lima <vitorliman@gmail.com>
2021-07-07 13:39:22 -0300

    Exercicio 1

--- exercicios/bloco-5.3/script.js
+++ exercicios/bloco-5.3/script.js
@@ -14,3 +14,13 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+
+let days = document.querySelector('#days');
+for (let index = 0; index < dezDaysList.length; index += 1) {
+  let weekDays = document.createElement('li');
+  weekDays.innerText = index;
+  weekDays.className = 'day';
+  days.appendChild(weekDays);
+}
