f8e3aaa434078bfa9c2b713f1792e77fc9b70744
Vitor Lima <vitorliman@gmail.com>
2022-01-11 09:01:31 -0300

    Implementa o readFile utilizando promises

--- exercicios/backEnd/bloco-22.2/io-local/readFile.js
+++ exercicios/backEnd/bloco-22.2/io-local/readFile.js
@@ -1,11 +1,12 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 
 const nomeDoArquivo = 'meu-arquivo.txt';
 
-fs.readFile(nomeDoArquivo, 'utf8', (err, data) => {
-  if (err) {
+fs.readFile(nomeDoArquivo, 'utf8')
+  .then((data) => {
+    console.log(`Conteudo do arquivo: ${data}`);
+  })
+  .catch((err) => {
     console.error(`Não foi possivel ler o arquivo ${nomeDoArquivo}\n Erro: ${err}`);
     process.exit(1);
-  }
-  console.log(`Conteudo do arquivo ${data}`);
-})
+  });
