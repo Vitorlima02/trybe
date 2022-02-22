61c3e22aefb7977d181f21fd6b81ee6b37160566
Vitor Lima <vitorliman@gmail.com>
2022-02-22 14:21:15 -0300

    Add exercicios

---
+++ exercicios/backEnd/bloco-26.3/express-typescript/routes/books.ts
@@ -0,0 +1,35 @@
+import { Router, Request, Response } from "express";
+import fs from 'fs/promises';
+import Book from "../interfaces/Book";
+
+const router = Router();
+
+router.get('/books', async (req: Request, res: Response) => {
+  const data = await fs.readFile('./books.json', 'utf8');
+
+  const books: Book[] = JSON.parse(data);
+
+  return res.status(200).json(books);
+});
+
+router.get('/books/:isbn', async (req: Request, res: Response) => {
+  const { isbn } = req.params;
+
+  const data = await fs.readFile('./books.json', 'utf8');
+
+  const books: Book[] = JSON.parse(data);
+
+  const book = books.find(book => book.isbn === isbn)
+
+  if (!book) return res.status(404).send('Livro não encontrado');
+
+  return res.status(200).json(book);
+});
+
+router.post('/books', (req: Request, res: Response) => {});
+
+router.put('/books/:isbn', (req: Request, res: Response) => {});
+
+router.delete('/books/:isbn', (req: Request, res: Response) => {});
+
+export default router;
