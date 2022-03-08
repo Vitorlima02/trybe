f094ee38311670ed2006957f80ea1a9432fd4755
Vitor Lima <vitorliman@gmail.com>
2022-03-08 19:22:13 -0300

    começa exerc do dia

---
+++ exercicios/backEnd/bloco-27.4/src/index.ts
@@ -0,0 +1,41 @@
+// ./src/index.ts
+
+type Discipline = {
+  name: string;
+  grade: number;
+  letterGrade?: string;
+};
+
+type Student = {
+  name: string;
+  disciplines: Discipline[];
+};
+
+// Converter
+
+const percentageGradesIntoLetters = ({ name, disciplines }: Student) => ({
+  name,
+  disciplines: disciplines.map(({ name, grade }) => {
+    let letterGrade;
+
+    if (grade >= 0.9) letterGrade = 'A';
+    else if (grade >= 0.8) letterGrade = 'B';
+    else if (grade >= 0.7) letterGrade = 'C';
+    else if (grade >= 0.6) letterGrade = 'D';
+    else if (grade >= 0.1) letterGrade = 'E';
+    else letterGrade = 'F';
+
+    return { name, grade, letterGrade };
+  })});
+
+// Determinar
+
+const approvedStudents = ({ disciplines }: Student): boolean => disciplines.every(({ grade }) => grade > 0.7);
+
+// Atualizar
+
+const updateApprovalData = ({ name: studentName, disciplines }: Student): void => {
+  console.log(`A pessoa com nome ${studentName} foi aprovada!`);
+
+  disciplines.map(())
+}
